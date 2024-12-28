f325aa571311243c79e1da9a246c0d83578fe94a
meriemrekik <rekik.meriem@gmail.com>
2024-12-28 18:06:45 +0100

    ouverture de menu

---
+++ app.js
@@ -0,0 +1,9 @@
+const btmnMenu = document.querySelector('.div btn-rond-menu');
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
+const ligne = document.querySelector('.cont-ligne');
+
+btmnMenu.addEventListener('click' ,  () =>{
+    
+
+})
