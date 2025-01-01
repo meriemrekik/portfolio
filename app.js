297228d4ad120fecb540a2d30c5b6b3f79953587
meriemrekik <rekik.meriem@gmail.com>
2025-01-01 16:30:05 +0100

    page d'acceuil

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const btmnMenu = document.querySelector('.div btn-rond-menu');
+const btnMenu = document.querySelector('.btn-rond-menu')
 const nav = document.querySelector('.nav-gauche');
 const allItemNav = document.querySelectorAll('.nav-menu-item');
 const ligne = document.querySelector('.cont-ligne');
 
-btmnMenu.addEventListener('click' ,  () =>{
-    
+btnMenu.addEventListener('click', () => {
+
+    ligne.classList.toggle('active')
+    nav.classList.toggle('menu-visible')
 
 })
+
+if (window.matchMedia('(max-width: 1300px)')){
+allItemNav.forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.toggle('menu-visible')
+        ligne.classList.toggle('active');
+        })
+})
+}
+
